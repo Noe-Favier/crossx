b7a78fd4af25cceb8be56865b433dc90ec155624
Noé <noe.favier@outlook.com>
2025-02-13 10:22:31 +0100

    wip(comment): added field

--- api/models/comment.go
+++ api/models/comment.go
@@ -9,9 +9,10 @@ type Comment struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 
-	Content string `json:"content"`
-	Post    Post   `json:"post" gorm:"foreignKey:PostID"`
-	User    User   `json:"user" gorm:"foreignKey:UserID"`
+	Content string    `json:"content"`
+	Post    Post      `json:"post" gorm:"foreignKey:PostID"`
+	User    User      `json:"user" gorm:"foreignKey:UserID"`
+	Comment []Comment `json:"comments" gorm:"foreignKey:CommentID"`
 
 	PostID uint `json:"post_id"`
 	UserID uint `json:"user_id"`
