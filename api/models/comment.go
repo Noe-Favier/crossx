d33c5466c1d1326e4839bb2d9d4296b47bf3a81f
Noé <noe.favier@outlook.com>
2025-02-13 11:30:30 +0100

    fx

--- api/models/comment.go
+++ api/models/comment.go
@@ -9,10 +9,9 @@ type Comment struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 
-	Content string    `json:"content"`
-	Post    Post      `json:"post" gorm:"foreignKey:PostID"`
-	User    User      `json:"user" gorm:"foreignKey:UserID"`
-	Comment []Comment `json:"comments" gorm:"foreignKey:CommentID"`
+	Content string `json:"content"`
+	Post    Post   `json:"post" gorm:"foreignKey:PostID"`
+	User    User   `json:"user" gorm:"foreignKey:UserID"`
 
 	PostID uint `json:"post_id"`
 	UserID uint `json:"user_id"`
